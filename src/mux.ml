50195724478b87798d859ee27bf20011222eb958
Markus Rudy <webmaster@burgerdev.de>
2018-04-21 22:57:58 +0200

    add type sig to mux module

--- src/mux.ml
+++ src/mux.ml
@@ -1,12 +1,15 @@
+open Cohttp_lwt
 open Cohttp_lwt_unix
+
+type handler = Server.conn -> Request.t -> Body.t -> (Response.t * Body.t) Lwt.t
 
 let not_found _conn _req _body =
   Server.respond_not_found ()
 
-let mux get_handler_opt conn req body =
+let mux: (Request.t -> handler option) -> handler = fun get_handler_opt conn req body ->
   match get_handler_opt req with
   | Some handler -> handler conn req body
   | None -> not_found conn req body
 
-let mux_path get_handler_opt =
+let mux_path: (string -> handler option) -> handler = fun get_handler_opt ->
   mux (fun req -> Request.uri req |> Uri.path |> get_handler_opt)
